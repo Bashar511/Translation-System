4fc1eb1cc35cb7cae73a602ec9d49366c749da55
yazan <yazankazaq0@gmail.com>
2023-12-27 12:58:54 +0300

    add db

--- backend/translationSite/models.py
+++ backend/translationSite/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class project(models.Model):
+        nameproject=models.CharField(max_length=50)
+        deliverytime=models.DateTimeField()
+        fileEN=models.FileField()
+        fileAR=models.FileField()
+
+
+class outputAI (models.Model):
+    nameproject=models.ForeignKey(project,related_name='output',on_delete=models.CASCADE)
+    sentenceEN=models.TextField(max_length=1000)
+    sentenceAR=models.TextField(max_length=1000)
+    starttime=models.TimeField()
+    endtime=models.TimeField()
+    sentence_status = [
+        ('correct translation','correct translation'),
+        ('not verified yet','not verified yet'),
+        ('re-translated','re-translated'),
+        
+    ]
+    status=models.CharField(max_length=25,choices=sentence_status,default='not verified yet')
+
+
+
+
