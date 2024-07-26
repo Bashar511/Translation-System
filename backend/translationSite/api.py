4455a6771e727e950d060ce656a6e56d2e0ddffa
Bashar Alloush <bashar.alloushf1@gmail.com>
2024-07-26 14:19:06 -0700

    fixed API call return 503, ignored migration

--- backend/translationSite/api.py
+++ backend/translationSite/api.py
@@ -1,33 +1,41 @@
 import json
 import requests
-
+import time
 
 def hel_en2ar(payload):
-    #headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    headers = {"Authorization": "Bearer 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"}
     API_URL = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-tc-big-en-ar"
     data = json.dumps(payload)
-    try:
-        response = requests.request("POST", API_URL, data=data)
-    except requests.exceptions.RequestException:
-        return "No internet connection"
-    if response.ok:
-        text = json.loads(response.content.decode("utf-8"))
-        return text[0]['translation_text']
-    else:
-        return response.reason
+    for attempt in range(3):
+        try:
+            response = requests.request("POST", API_URL, data=data)
+        except requests.exceptions.RequestException:
+            return "No internet connection"
+        if response.status_code == 503:
+            time.sleep(5)
+            pass
+        if response.ok:
+            text = json.loads(response.content.decode("utf-8"))
+            return text[0]['translation_text']
+        else:
+            return response.reason
     
 def bart_en2ar(payload):
-    #headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    headers = {"Authorization": "Bearer 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"}
     API_URL = "https://api-inference.huggingface.co/models/facebook/mbart-large-50-many-to-many-mmt"
     #data = json.dumps(payload)
     med = {"inputs": payload, "parameters": {"src_lang": "en_XX", "tgt_lang": "ar_AR"}}
     data = json.dumps(med)
-    try:
-        response = requests.request("POST", API_URL, data=data)
-    except requests.exceptions.RequestException:
-        return "No internet connection"
-    if response.ok:
-        text = json.loads(response.content.decode("utf-8"))
-        return text[0]['translation_text']
-    else:
-        return response.reason
+    for attempt in range(3):
+        try:
+            response = requests.request("POST", API_URL, data=data, headers=headers)
+        except requests.exceptions.RequestException:
+            return "No internet connection"
+        if response.status_code == 503:
+            time.sleep(5)
+            pass
+        if response.ok:
+            text = json.loads(response.content.decode("utf-8"))
+            return text[0]['translation_text']
+        else:
+            return response.reason
