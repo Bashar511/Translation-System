df65484d0288fd861af0de78c107fe5331cbfb0d
yzan99 <yazankazaq0@gmail.com>
2024-08-11 07:56:58 -0700

    CREATE  Page - Frontend

--- backend/translationSite/forms.py
+++ backend/translationSite/forms.py
@@ -7,6 +7,54 @@ class projectForm(forms.ModelForm):
         class Meta:
             model = Project
             fields = ['title', 'publish','deliverytime','fileEN']
+            widgets={
+            
+            'title': forms.TextInput(attrs={
+            'id':'Title',
+            'class': 'form-create',
+            
+            }),
+            'publish':forms.TextInput(attrs={
+            'id':'publish',
+            'class': 'form-create',
+            
+            }),
+            'deliverytime':forms.TextInput(attrs={
+            'id':'deliverytime',
+            'class': 'form-create',
+            
+            }),
+            'fileEN': forms.FileInput(attrs={
+            'id':'fileEN',
+            'class': 'form-create'}),}
+            labels = {'fileEN': 'File',}
+    
+        def __init__(self, *args, **kwargs):
+            super(projectForm, self).__init__(*args, **kwargs)
+            # Adding custom class to the label of the 'photo' field
+            self.fields['fileEN'].label = 'File'
+            self.fields['fileEN'].widget.attrs.update({'class': 'form-control-file'})
+            self.fields['fileEN'].widget.attrs.update({'label_class': 'custom-label-class'})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class PermissionForm(forms.ModelForm):
     class Meta:
