b236b3bbc0d0a66fd382d7afe398ea39612280a8
yzan99 <yazankazaq0@gmail.com>
2024-08-12 02:15:36 -0700

    Select to upload only srt files.

--- backend/translationSite/models.py
+++ backend/translationSite/models.py
@@ -1,15 +1,22 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.contrib.auth.models import User
+import os
+
 # db for project
+
+
+    
 class Project(models.Model):
         title=models.CharField(max_length=50)
         owner = models.ForeignKey(User, related_name='owned_projects', on_delete=models.CASCADE)
         publish = models.DateTimeField(default=timezone.now)
         deliverytime=models.DateField()
         fileAR=models.FileField(upload_to='input',null=True)
-        fileEN=models.FileField(upload_to='input' )
+        fileEN=models.FileField(upload_to='input')
         updated_dt = models.DateTimeField(null=True)
 
         class Meta:
@@ -20,9 +27,17 @@ class Project(models.Model):
         def __str__(self):
             return self.title
 
+        def clean(self):
+                super().clean()
+                if self.fileEN:
+                    ext = os.path.splitext(self.fileEN.name)[1].lower()
+                    if ext != '.srt':
+                        raise ValidationError({'fileEN': _('فقط ملفات .srt مسموح بها.')})
 
+        def save(self, *args, **kwargs):
+                self.clean()
+                super().save(*args, **kwargs)
 
 class ProjectMember(models.Model):
     granted_to = models.ForeignKey(User, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-
